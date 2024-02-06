5e6b4c45ead2df81eba5a548035c9a4b00ed2bb5
Dmytro Pahuba <dpaguba@gmail.com>
2024-02-06 21:28:05 +0100

    add app-routing to navigate to leaderboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component'; // Import the missing 'LeaderboardComponent' class
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomepageComponent,
-    title: 'Home',
+    data: { title: 'Home' },
+  },
+  {
+    path: 'leaderboard',
+    component: LeaderboardComponent,
+    data: { title: 'Leaderboard' }, 
   },
   { path: '**', redirectTo: 'home' },
 ];
