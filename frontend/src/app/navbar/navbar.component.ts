b95901a565f44a86893cf0ffd57962a51b07dc68
vhudym <vladyslav.hudym@westcode.de>
2024-02-06 22:34:15 +0100

    feat(homepage): responsive navbar + menu

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private elRef: ElementRef) {}
+
+  toggleMenu(): void {
+    this.elRef.nativeElement.classList.toggle('active');
+  }
+}
