582c2158a2b045e146cb69bc6dd1e3dcbac4555e
vhudym <vladyslav.hudym@westcode.de>
2024-02-07 20:16:22 +0100

    feat: orbpage

---
+++ frontend/src/app/orbpage/orbpage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'orbpage',
+  templateUrl: './orbpage.component.html',
+  styleUrl: './orbpage.component.scss',
+})
+export class OrbpageComponent {}
