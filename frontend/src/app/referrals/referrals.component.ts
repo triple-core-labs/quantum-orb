1aa9006de86e93c2cd4b75a343a847d41cb4be62
vhudym <vladyslav.hudym@westcode.de>
2024-02-13 20:12:19 +0100

    feat: referrals page

--- frontend/src/app/referrals/referrals.component.ts
+++ frontend/src/app/referrals/referrals.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { AppSelectors } from '../store/app/app.selectors';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+import { CommonModule } from '@angular/common';
+import { ShortAddressPipe } from '../pipes/short-address.pipe';
 
 @Component({
   selector: 'app-referrals',
   templateUrl: './referrals.component.html',
   styleUrl: './referrals.component.scss',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ShortAddressPipe],
 })
-export class ReferralsComponent {}
+export class ReferralsComponent implements OnInit {
+  @Select(AppSelectors.referrals)
+  referrals$!: Observable<User[]>;
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  innerWidth: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+}
