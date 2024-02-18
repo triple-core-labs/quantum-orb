8233920789ace8d87778625ade79f2bef72ae5af
cashblxxd <cashblxxd@gmail.com>
2024-02-18 21:15:43 +0100

    feat: pre-commit init & stale x integration

--- contracts/contracts/QuantumOrb.sol
+++ contracts/contracts/QuantumOrb.sol
@@ -11,7 +11,7 @@ enum YieldMode {
 
 enum GasMode {
     VOID,
-    CLAIMABLE 
+    CLAIMABLE
 }
 
 interface IBlast{
@@ -151,7 +151,7 @@ contract QuantumOrb is Initializable {
 
         users[msg.sender].lastOpenedDaily = block.timestamp;
         uint pointsEarned;
-        
+
         uint8 rank = getOrbRank();
 
         if (rank == 1) {
@@ -172,7 +172,7 @@ contract QuantumOrb is Initializable {
     function openGenesisOrb() external payable returns (uint) {
         require(msg.value >= 0.0015 ether, "Insufficient ETH sent for Genesis Orb, 0.0015 ETH required");
         uint pointsEarned;
-        
+
         uint8 rank = getOrbRank();
         uint rand = getRand();
 
@@ -194,7 +194,7 @@ contract QuantumOrb is Initializable {
     function openQuantumOrb() external payable returns (uint) {
         require(msg.value >= 0.0027 ether, "Insufficient ETH sent for Quantum Orb, 0.0027 ETH required");
         uint pointsEarned;
-        
+
         uint8 rank = getOrbRank();
         uint rand = getRand();
 
