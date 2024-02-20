909d11a68948f0def06c57123aa440d7fbc55666
Ivan Pushkarev <cashblxxd@gmail.com>
2024-02-20 15:49:30 +0100

    fix: wildcard ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-mt@gtc02jq!51ur5n(t*x(t*24xjinf5tde#eon4lvd4a)i!ys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quantum-orb.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
