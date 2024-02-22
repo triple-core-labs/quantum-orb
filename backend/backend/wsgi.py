9733c5122816dc3fc7f3e990ed2e590a450baf52
cashblxxd <cashblxxd@gmail.com>
2024-02-22 11:04:28 +0100

    fix: simplify scheduling

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -10,19 +10,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.signals import request_started
+from django.core.signals import ready
 from django.dispatch import receiver
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
 
-@receiver(request_started)
-def schedule_task(sender, **kwargs):
-    from background_task.models import Task
-    from backend.leaderboard.tasks import update_top_blast_addresses
+from background_task.models import Task
+from backend.leaderboard.tasks import update_top_blast_addresses
 
-    # Check if the task is already scheduled
-    if not Task.objects.filter(task_name='leaderboard.tasks.update_top_blast_addresses').exists():
-        # If not, schedule the task
-        update_top_blast_addresses(repeat=60)
+# Check if the task is already scheduled
+if not Task.objects.filter(task_name='backend.leaderboard.tasks.update_top_blast_addresses').exists():
+    # If not, schedule the task
+    update_top_blast_addresses(repeat=60)
