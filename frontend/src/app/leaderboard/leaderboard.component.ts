f5079a9f5ccec18daa871e2243798e0a46656b4e
Dmytro Pahuba <dpaguba@gmail.com>
2024-05-03 12:18:50 +0200

    feat(frontend): show global stats and top inviters on the leaderboard

--- frontend/src/app/leaderboard/leaderboard.component.ts
+++ frontend/src/app/leaderboard/leaderboard.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit, computed, inject } from "@angular/core";
+import { Component, OnInit, computed, inject, signal } from "@angular/core";
 import { Store } from "@ngxs/store";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { AppSelectors } from "../store/app/app.selectors";
 import { LoadLeaderboard } from "../store/app/app.actions";
 import { ShortAddressPipe } from "../pipes/short-address.pipe";
+import { ActivityFeedComponent } from "../activity/activity-feed.component";
+import { ApiService } from "../api/api.service";
+import { ReferrerRow } from "../api/api.types";
 
 @Component({
   selector: "app-leaderboard",
   standalone: true,
-  imports: [ShortAddressPipe],
+  imports: [ShortAddressPipe, ActivityFeedComponent],
   templateUrl: "./leaderboard.component.html",
   styleUrl: "./leaderboard.component.scss",
 })
 export class LeaderboardComponent implements OnInit {
   private readonly store = inject(Store);
+  private readonly api = inject(ApiService);
+
+  readonly referrers = signal<ReferrerRow[]>([]);
 
   readonly top = toSignal(this.store.select(AppSelectors.leaderboard), {
     initialValue: [],
@@ -29,6 +35,9 @@ export class LeaderboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(new LoadLeaderboard());
+    this.api.referrers().subscribe(({ referrers }) =>
+      this.referrers.set(referrers),
+    );
   }
 
   isYou(rowAddress: string): boolean {
